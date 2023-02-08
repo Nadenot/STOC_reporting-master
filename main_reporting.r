d5ab1251e0ca364943aeda51f14f79315268d1d8
Nadenot <73333494+Nadenot@users.noreply.github.com>
2023-02-08 14:38:20 +0100

    Create main_reporting.r

---
+++ main_reporting.r
@@ -0,0 +1,8 @@
+source("functions/fun_generic_latin1.r")
+source("functions/fun_indicateurs_france_latin1.r")
+source("functions/fun_indicateurs_local_latin1.r")
+source("functions/fun_rmd_latin1.r")
+source("functions/fun_mainSTOCreporting_latin1.r")
+source("functions/fun_stoc_preparation_data_latin1.r")
+
+mainSTOCreporting(importationData="3sessions",all=TRUE,local=TRUE,site=NULL,pdf_local=TRUE,abondanceRelative=TRUE ,variationAbondance=TRUE,variationAbondanceEspece=TRUE,productivite=TRUE,productiviteEspece=TRUE,conditionCorporelle=TRUE,retour=TRUE,onlyNew=FALSE)
