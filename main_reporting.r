ece72eebfa76b9e0eda13f19f2675b637f8b4b00
Nadenot <73333494+Nadenot@users.noreply.github.com>
2023-02-21 16:32:50 +0100

    Update main_reporting.r

--- main_reporting.r
+++ main_reporting.r
@@ -4,5 +4,7 @@ source("functions/fun_indicateurs_local_latin1.r")
 source("functions/fun_rmd_latin1.r")
 source("functions/fun_mainSTOCreporting_latin1.r")
 source("functions/fun_stoc_preparation_data_latin1.r")
+source("functions/fun_biogeo.r")
+
 
 mainSTOCreporting(importationData="3sessions",all=TRUE,local=TRUE,site=NULL,pdf_local=TRUE,abondanceRelative=TRUE ,variationAbondance=TRUE,variationAbondanceEspece=TRUE,productivite=TRUE,productiviteEspece=TRUE,conditionCorporelle=TRUE,retour=TRUE,onlyNew=FALSE)
